0cb317c5d40d0273419473285b154104f5e50750
Jonathan Rascher <jon@bcat.name>
2011-10-06 13:00:52 -0500

    Make prefork stuff work; add dummy response code

--- seamonster.c
+++ seamonster.c
@@ -17,55 +17,22 @@ int main(int argc, char **argv);
 static int create_passive_sock(short port, int backlog);
 static void dispose_passive_sock(int sock);
 
-static pid_t *fork_workers(size_t num);
-static void dispose_workers(pid_t *pids, size_t num);
+static pid_t *create_workers(size_t num_workers, int passive_sock);
+static void dispose_workers(pid_t *pids);
 
-pid_t *fork_workers(size_t num) {
-  size_t i;
-  pid_t *pids = malloc(num * sizeof(pid_t));
+static int worker_main(int passive_sock);
 
-  if (!pids) {
-    return NULL;
-  }
-
-  for (i = 0; i < num; ++i) {
-    switch (pids[i] = fork()) {
-      case -1:
-        free(pids);
-        return NULL;
-
-      case 0:
-        /* TODO: Implement worker functionality. */
-        exit(0);
-    }
-  }
-
-  return pids;
-}
-
-void dispose_workers(pid_t *pids, size_t num) {
-  if (pids) {
-    for (;;) {
-      if (wait(NULL) == -1) {
-        if (errno == ECHILD) {
-          break;
-        }
-
-        if (errno != EINTR) {
-          perror("Couldn't wait for workers to die");
-        }
-      }
-    }
-
-    free(pids);
-  }
-}
+static void handle_request(int sock);
 
 int create_passive_sock(short port, int backlog) {
-  int sock;
+  int one = 1, sock;
   struct sockaddr_in addr = { 0 };
 
   if ((sock = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
+    return -1;
+  }
+
+  if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one)) == -1) {
     return -1;
   }
 
@@ -96,16 +63,66 @@ void dispose_passive_sock(int sock) {
   }
 }
 
-int main(int argc, char **argv) {
-  int ret, exit_status = 0, passive_sock = -1;
-  pid_t *worker_pids = NULL;
+pid_t *create_workers(size_t num_workers, int passive_sock) {
+  size_t i;
+  pid_t *pids = malloc(num_workers * sizeof(pid_t));
 
-  /* Fork off some children to handle clients. */
-  if (!(worker_pids = fork_workers(DEFAULT_WORKERS))) {
-    perror("Couldn't fork workers to handle connections");
-    exit_status = 1;
-    goto cleanup;
+  if (!pids) {
+    return NULL;
   }
+
+  for (i = 0; i < num_workers; ++i) {
+    switch (pids[i] = fork()) {
+      case -1:
+        free(pids);
+        return NULL;
+
+      case 0:
+        exit(worker_main(passive_sock));
+    }
+  }
+
+  return pids;
+}
+
+void dispose_workers(pid_t *pids) {
+  free(pids);
+}
+
+int worker_main(int passive_sock) {
+  /* Process connections until interrupted the parent is interrupted. */
+  for (;;) {
+    int ret, conn_sock;
+
+    /* Accept an incoming connection request. */
+    while ((conn_sock = accept(passive_sock, NULL, NULL)) == -1
+        && errno == EINTR);
+
+    if (conn_sock == -1) {
+      perror("Couldn't accept connection request");
+      return 1;
+    }
+
+    /* Handle the new client connection. */
+    handle_request(conn_sock);
+
+    /* Close the client connection's socket. */
+    while ((ret = close(conn_sock) == -1 && errno == EINTR));
+
+    if (ret == -1) {
+      perror("Couldn't close connection socket");
+    }
+  }
+}
+
+void handle_request(int sock) {
+  char msg[] = "iHello, world of Gopher!\n.\n";
+  send(sock, msg, sizeof(msg), 0);
+}
+
+int main(int argc, char **argv) {
+  int exit_status = 0, passive_sock = -1;
+  pid_t *worker_pids = NULL;
 
   /* Create a passive socket to listen for connection requests. */
   if ((passive_sock =
@@ -115,30 +132,30 @@ int main(int argc, char **argv) {
     goto cleanup;
   }
 
-  /* Accept new connections until interrupted. */
+  /* Fork off some children to handle clients. */
+  if (!(worker_pids = create_workers(DEFAULT_WORKERS, passive_sock))) {
+    perror("Couldn't fork workers to handle connections");
+    exit_status = 1;
+    goto cleanup;
+  }
+
+  /* Wait for all our children to die. */
   for (;;) {
-    int conn_sock;
+    if (wait(NULL) == -1) {
+      if (errno == ECHILD) {
+        break;
+      }
 
-    while ((conn_sock = accept(passive_sock, NULL, NULL)) == -1
-        && errno == EINTR);
-
-    if (conn_sock == -1) {
-      perror("Couldn't accept connection request");
-      exit_status = 1;
-      goto cleanup;
-    }
-
-    while ((ret = close(conn_sock) == -1 && errno == EINTR));
-
-    if (ret == -1) {
-      perror("Couldn't close connection socket");
+      if (errno != EINTR) {
+        perror("Couldn't wait for workers to die");
+      }
     }
   }
 
   /* Clean up after ourselves. */
 cleanup:
+  dispose_workers(worker_pids);
   dispose_passive_sock(passive_sock);
-  dispose_workers(worker_pids, DEFAULT_WORKERS);
 
   return exit_status;
 }
